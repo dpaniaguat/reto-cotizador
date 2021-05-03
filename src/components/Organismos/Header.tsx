5197c0e6f088ee38b2e3a3c93b43ba156fef387d
Darwin Paniagua Tasayco <82116931+dpaniaguat@users.noreply.github.com>
2021-05-03 09:18:27 -0500

    update dev all pages

--- src/components/Organismos/Header.tsx
+++ src/components/Organismos/Header.tsx
@@ -4,7 +4,10 @@ import { Component } from 'react';
 export const  Header =()=>{
 
     return (
-        <div className="header-app dos">
+        <div className="header-app">
+            <div className="cotiza__left_top__logo">
+              <img src="/src/resources/imgs/logo.png" alt="" className=""/>
+            </div>
             <div className="cotiza__right_header" >
                 <span>¿Tienes alguna duda?</span>
             </div>
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,41 +1,53 @@
 import { FCotiza } from '../Organismos/FCotiza';
 
-export const Index=()=>{
+export const Index = () =>{
 
     return (
-        <>
-          <div className="cotiza login">
-            <div className="cotiza__left_top__logo">
-              <img src="/src/resources/imgs/logo.png" alt="" className=""/>
+        
+          <div className="cotiza_login">
+            
+            <div className="cotiza_left">
+              <div className="cotiza__left_top">
+                <div className="cotiza__left_top__center">
+                  <img src="/src/resources/imgs/leftopcenter.png" alt="" className=""/>
+                </div>
+              </div>
+
+              <div className="cotiza__left_botton">
+                <span className="cotiza__left_botton_titulo">NUEVO</span>
+                <div className="cotiza__left_botton_center">
+                  <span>Seguro Vehicular Tracking</span>
+                </div>
+                <div className="cotiza__left_botton_subtitle">
+                  <span>
+                          Cuentanos donde le haras seguimiento a tu seguro
+                          </span>
+                </div>
+              </div>
+
             </div>
-            <div className="cotiza__left_top">
-              <div className="cotiza__left_top__center">
-                <img src="/src/resources/imgs/leftopcenter.png" alt="" className=""/>
-              </div>
+            
+            <div className="cotiza_right">
+
+                <div className="cotiza__right_titulo">
+                      <span>Déjanos tus datos</span>
+                </div> 
+                <div className="cotiza_right_form">
+                  <FCotiza/>
+                </div>
             </div>
-            <div className="cotiza__left_botton">
-              <span className="cotiza__left_botton_titulo">NUEVO</span>
-              <div className="cotiza__left_botton_center">
-                <span>Seguro Vehicular Tracking</span>
-              </div>
-              <div className="cotiza__left_botton_subtitle">
-                <span>
-                        Cuentanos donde le haras seguimiento a tu seguro
-                        </span>
-              </div>
-            </div>
-            <div>
-              
-              <div className="cotiza__right_titulo">
-                <span>Déjanos tus datos</span>
-              </div>
-              <div className="cotiza_right_form">
-                <FCotiza/>
-              </div>
-            </div>
-          </div>
           
-        </>
+            <div className="cotiza_mobile">
+              <div className="cotiza_up_mobile">
+
+              </div>
+              <div className="cotiza_down_mobile">
+
+              </div>
+            </div>
+
+        </div>
+        
       );
     }
 
